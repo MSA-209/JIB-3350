90d06a96692b98a8cc924a938b3c1c08d917cf21
rza628 <ryanzhang2468@gmail.com>
2024-01-26 04:15:15 -0500

    Implemented youtube video embed, functions still work but layout is a little messy

--- VideoButton.js
+++ VideoButton.js
@@ -1,9 +1,59 @@
-import React from 'react';
-import { View, Image, TouchableOpacity, Text, Linking, ScrollView } from 'react-native';
+import React, { useState, useCallback, useRef } from 'react';
+import { StyleSheet, View, Image, TouchableOpacity, Text, Linking, ScrollView , Alert, Button} from 'react-native';
 import {IconButton} from 'react-native-paper'
 import { AddedVideosContext } from './videoContext';
+ 
+  
+import YoutubePlayer from "react-native-youtube-iframe";
 
+const styles = StyleSheet.create(
+  {
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+  }
+)
+function VideoComp({video, playing, handleAddToPlaylist, addedVideos}){
+
+  return (
+    
+    <View style={{backgroundColor: '#ffcc01', width: 320,   alignSelf: 'center', 
+    borderBottomLeftRadius:8, borderBottomRightRadius: 8, 
+    borderTopRightRadius:8,borderTopLeftRadius:8,
+    marginBottom: 10, alignItems: 'center', justifyContent: 'center' }}>
+       <View style={{flexDirection: 'row', justifyContent: 'space-between', marginBottom: 10}} >
+        
+        <Text style={{ fontSize: 16, fontWeight: 'bold', alignSelf: 'center', padding: 10 }}>
+                {video.title}
+      </Text>
+    <TouchableOpacity onPress={() => handleAddToPlaylist(video)}  style={{ flexDirection: 'row', alignItems: 'center' }}>
+    <Text style={{ fontSize: 16, fontWeight: 'bold', alignSelf: 'center', padding: 5 }}>
+                  { addedVideos[video.link] ? 'Remove from' :'Add to'}</Text>
+      <IconButton
+      icon="playlist-play"
+      size={20}
+      color="#000000"
+    />
+    </TouchableOpacity>
+      
+       </View>
+      <YoutubePlayer
+        height={200}
+        width={300}
+        play={playing}
+        videoId={video.id}
+        />
+    </View>
+  );
+
+}
 const VideoButton = ({ videoLinks }) => {
+
+  const [playing, setPlaying] = useState(false);
+   
+
   const { addedVideos, setAddedVideos } = React.useContext(AddedVideosContext);
 
   const handleVideoPress = (video) => {
@@ -19,12 +69,41 @@ const VideoButton = ({ videoLinks }) => {
     }));
     console.log(addedVideos)
   };
-
+   
+  
   return (
+     
+     <View style ={styles.container}>
+       
+         {videoLinks.map((video) => (
+          <VideoComp key={video.id} video={video}
+          handleAddToPlaylist={handleAddToPlaylist} 
+          addedVideos={addedVideos}
+          />
+         ))}
+    </View>
+      
+     /* <View style ={styles.container}>
+      
+      <YoutubePlayer
+        height={300}
+        width={300}
+        play={playing}
+        videoId={"fNk_zzaMoSs"}
+        />
+         
+    </View>
+     */
+  
+
+    /*
     <ScrollView vertical showsVerticalScrollIndicator={true}>
       {videoLinks.map((video, index) => (
         <View key={index} style={{ marginRight: 16 }}>
-          <TouchableOpacity onPress={() => handleVideoPress(video)} style={{ width: 320, alignSelf: 'center' }}>
+
+  
+
+      {<TouchableOpacity onPress={() => handleVideoPress(video)} style={{ width: 320, alignSelf: 'center' }}>
             <Image
               source={{ uri: `https://img.youtube.com/vi/${video.link.split('v=')[1]}/0.jpg` }}
               style={{ marginTop: 20, width: 320, height: 180, alignSelf: 'center', borderTopLeftRadius: 8, borderBottomRightRadius: 8 }}
@@ -33,7 +112,7 @@ const VideoButton = ({ videoLinks }) => {
               <Text style={{ fontSize: 16, fontWeight: 'bold', alignSelf: 'center', padding: 5 }}>
                 {video.title}
               </Text>
-              {/* Below changes depending on if user added it to playlist already or not*/}
+              {/* Below changes depending on if user added it to playlist already or not}
               <TouchableOpacity onPress={() => handleAddToPlaylist(video)} style={{ flexDirection: 'row', alignItems: 'center' }}>
                 <Text style={{ fontSize: 16, fontWeight: 'bold', alignSelf: 'center', padding: 5 }}>
                   {addedVideos[video.link] ? 'Remove from' : 'Add to'}
@@ -45,10 +124,14 @@ const VideoButton = ({ videoLinks }) => {
                 />
               </TouchableOpacity>
             </View>
-          </TouchableOpacity>
+          *</TouchableOpacity>}
+        
+          
+          
         </View>
       ))}
     </ScrollView>
+    */
   );
 };
 
--- videoLinks.js
+++ videoLinks.js
@@ -1,31 +1,37 @@
 const videoLinks = [
     {
       link: 'https://www.youtube.com/watch?v=jfKfPfyJRdk',
+      id: 'jfKfPfyJRdk',
       title: 'Video 1',
       description: 'Description for Video 1',
     },
     {
       link: 'https://www.youtube.com/watch?v=fNk_zzaMoSs',
+      id: 'fNk_zzaMoSs',
       title: 'Linear Algebra',
       description: 'matrix',
     },
     {
       link: 'https://www.youtube.com/watch?v=k7RM-ot2NWY',
+      id: 'k7RM-ot2NWY',
       title: 'Video 3',
       description: 'Description for Video 3',
     },
     {
       link: 'https://www.youtube.com/watch?v=XkY2DOUCWMU',
+      id: 'XkY2DOUCWMU',
       title: 'Video 4',
       description: 'Description for Video 4',
     },
     {
       link: 'https://www.youtube.com/watch?v=rHLEWRxRGiM',
+      id: 'rHLEWRxRGiM',
       title: 'Video 5',
       description: 'Description for Video 5',
     },
     {
       link: 'https://www.youtube.com/watch?v=Ip3X9LOh2dk',
+      id: 'Ip3X9LOh2dk',
       title: 'Video 6',
       description: 'Description for Video 6',
     },
